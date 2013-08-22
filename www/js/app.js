7df356dff0bb0f3ce641b9cbaf1488051a58b007
rormartin <rormartin@gmail.com>
2013-08-22 17:08:09 +0200

    Primera versión con equeleto para todas las secciones
    Listo para desplegar en AppEngine. Con otra infraestructura, con
    utilizar el directorio www sería más que suficiente.
    
    Bootstrap + AngularJS. Las secciones divididas en ficheros
    independientes (dentro de 'partials') para poder ser editadas.

---
+++ www/js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+angular.module('devfest', []).
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+            when('/agenda', {templateUrl: 'partials/agenda.html'}).
+            when('/localizacion', {templateUrl: 'partials/localizacion.html'}).
+            when('/inscripcion', {templateUrl: 'partials/inscripcion.html'}).
+            when('/otros_devfest', {templateUrl: 'partials/otros_devfest.html'}).
+            when('/inicio', {templateUrl: 'partials/inicio.html'}).
+            otherwise({redirectTo: '/inicio'});
+    }]);
